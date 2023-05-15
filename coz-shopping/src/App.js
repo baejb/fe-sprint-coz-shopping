59b86a3ac31904cba6026bbeed9ec0a75827e61f
baejb <bjb6478@naver.com>
2023-05-15 15:59:55 +0900

    데이터 가져오기

--- coz-shopping/src/App.js
+++ coz-shopping/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
+  const [datas , setdatas] = useState([]);
+
+  useEffect(() => {
+    fetch("http://cozshopping.codestates-seb.link/api/v1/products")
+    .then((res) => res.json())
+    .then((data) => setdatas(data))
+  },[])
+
   return (
     <div className="App">
-     
+     {console.log(datas[0].title)}
     </div>
   );
 }
---
+++ coz-shopping/src/TypeComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function TypeComponent(){
+    return(
+        <div>
+            hi
+        </div>
+    )
+
+}export default TypeComponent;
