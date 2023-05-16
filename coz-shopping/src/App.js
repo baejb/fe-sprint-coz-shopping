66350e41eba6acbbb7969b4624f5a1ce1469fca9
baejb <bjb6478@naver.com>
2023-05-17 03:10:01 +0900

    머지 완료

--- coz-shopping/src/App.js
+++ coz-shopping/src/App.js
@@ -1,14 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
-
-   
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from "./pages/MainPage"
 import ProductsListPage from './pages/ProductsListPage';
 import BookMarkPage from './pages/BookMarkPage';
 import Header from './Header';
-import Footer from './Footer';
 function App() {
   return (
     <div className="App">
@@ -20,8 +16,7 @@ function App() {
         <Route path='/bookmark' element= {<BookMarkPage/>} />
       </Routes>
      </Router>
-     <Footer/>
-
+     
     </div>
   );
 }
